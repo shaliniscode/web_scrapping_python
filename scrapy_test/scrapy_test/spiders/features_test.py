44b7aeb6f4c3a68b400df2f889858c44b848ca6c
sm-299 <52206733+sm-299@users.noreply.github.com>
2022-08-15 15:54:46 +0800

    Finished scrapy code

--- scrapy_test/scrapy_test/spiders/features_test.py
+++ scrapy_test/scrapy_test/spiders/features_test.py
@@ -5,37 +5,38 @@ import pandas as pd
 from scrapy.crawler import CrawlerProcess
 
 
-
+scrapped_info = {}
+desc=[]
 class FeaturesTestSpider(scrapy.Spider):
     name = 'features_test'
     allowed_domains = ['www.octoparse.com']
     start_urls = ['http://www.octoparse.com/']
     
-    process = CrawlerProcess(settings={
-    "FEEDS": {
-        "items.csv": {"format": "csv"},
-    },
-        })
-
+   
     #custom_settings: {"FEEDS": {"features.csv": {"format": "csv" }}}
     
     def parse(self, response):
-        print("processing:"+response.url)
-        #Extract data
-        heading =[]
-        description =[]
-    
-        #title = response.xpath("//section[@class='flex flex-column']/h2/a/text()").getall()
-        heading.append(response.xpath("//div[@class='block flex-one']/h3/text()").extract())
-        description.append(response.xpath("//div[@class='block flex-one']/p/text()").extract())
         
-        scrapped_info = pd.DataFrame({'features':heading, 'Description':description})
-             
+        #Extract data
+        heading = response.xpath("//div[@class='block flex-one']/h3/text()").extract()
+        description = response.xpath('//div[@class="block flex-one"]/p').extract()
+
+        for item in description:
+           desc.append(( item.strip('<p>,</p>')).replace('<br>'," "))
+     
+        scrapped_info = dict(zip(heading, desc))
+        print(scrapped_info)
+
+    
         yield scrapped_info
+        
 
 
 if __name__ == "__main__":
 
-    process = CrawlerProcess()
+    process = CrawlerProcess(settings={
+    "FEEDS": {
+        "features.csv": {"format": "csv"},
+    }})
     process.crawl(FeaturesTestSpider)
     process.start()
